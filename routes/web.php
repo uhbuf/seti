3f75ab4360ed8fde69089c29a75ddde952e889a1
uhbuf <griga01@mail.ru>
2022-09-30 10:55:05 +0300

    Changes to be committed: 	modified:   app/Http/Kernel.php 	modified:   app/Http/Middleware/Authenticate.php 	modified:   app/Models/User.php 	modified:   database/migrations/2014_10_12_000000_create_users_table.php 	modified:   resources/views/welcome.blade.php 	modified:   routes/web.php
     Untracked files:
    	app/Http/Controllers/LoginController.php
    	app/Http/Controllers/ProductController.php
    	app/Http/Controllers/RegistrController.php
    	app/Models/contact.php
    	database/migrations/2022_09_27_211805_create_contacts_table.php
    	public/css/
    	public/img/
    	resources/views/catalog.blade.php
    	resources/views/header.blade.php
    	resources/views/home.blade.php
    	resources/views/login.blade.php
    	resources/views/private.blade.php
    	resources/views/registration.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistrController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/catalog', function () {
+    return view('catalog');
+});
+
+Route::name('user.')->group(function(){
+    Route::view('/private', 'private')->middleware('auth')->name('private');
+    Route::get('/login',function(){
+        if(Auth::check()){
+            return redirect(route('user.private'));
+        }
+        return view('login');
+    })->name('login');
+
+    Route::get('/logout',function(){
+        Auth::logout();
+        return redirect('/');
+    })->name('logout');
+
+    Route::post('/login',[LoginController::class,'login']);
+
+    Route::get('/registration',function(){
+        if(Auth::check()){
+            return redirect(route('user.private'));
+        }
+        return view('registration');
+    })->name('registration');
+    Route::post('/registration',[RegistrController::class,'save']);
 });
